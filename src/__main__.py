c82a375a65b66c2527dbd4db46271b350a834585
Mikey Lau <mikey.lau@traininblocks.com>
2022-02-19 17:06:27 +0000

    feat: Add histogram with dynamic subplots

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,17 @@
 """Main runtime."""
 
 from process import preview_raw, get_cols
+from visualise import plot_hist
 
 if __name__ == "__main__":
     preview_raw()
 
-    df = get_cols(["histogram_mean", "baseline value"], as_df=True)
-    print(df)
+    df = get_cols(["baseline value", "fetal_health"], as_df=True)
+
+    X = [
+        df[df["fetal_health"] == 1]["baseline value"].values,
+        df[df["fetal_health"] == 2]["baseline value"].values,
+        df[df["fetal_health"] == 3]["baseline value"].values,
+    ]
+
+    plot_hist(X)
--- src/visualise.py
+++ src/visualise.py
@@ -13,3 +13,17 @@ def plot_scatter(x_values, y_values):
 
     plt.scatter(x_values, y_values)
     plt.show()
+
+
+def plot_hist(x_values):
+    """Generates histogram(s) from given values.
+
+    Args:
+        x_values (list[list[float]]): A matrix of values.
+    """
+
+    fig, axs = plt.subplots(1, len(x_values), figsize=(12, 9))
+    for index, x in enumerate(x_values):
+        axs[index].hist(x)
+    fig.suptitle("Histogram plot")
+    plt.show()
