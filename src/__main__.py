a1a8cf839f21d67d904ddeb3e1aeae8431955ec5
Mikey Lau <mikey.lau@traininblocks.com>
2022-02-19 16:29:33 +0000

    feat(process): Better get_cols with option to return a Dataframe

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,9 @@
 """Main runtime."""
 
 from process import preview_raw, get_cols
-from visualise import plot_scatter
 
 if __name__ == "__main__":
     preview_raw()
 
-    acc, base = get_cols(["histogram_mean", "baseline value"])
-
-    plot_scatter(acc, base)
+    df = get_cols(["histogram_mean", "baseline value"], as_df=True)
+    print(df)
--- src/process.py
+++ src/process.py
@@ -18,19 +18,21 @@ def preview_raw():
     print(raw_df.head())
 
 
-def get_cols(features=None):
+def get_cols(features=None, as_df=False):
     """Gets the specified columns or returns the entire dataFrame.
 
     Args:
         features (List[str], optional): A list of features to pull from the raw dataframe. Defaults to None.
+        in_row (bool, optional): To return as a row. Defaults to True.
 
     Returns:
         Tuple[ndarray[float]]: A tuple of features with its respective values.
+        Dataframe: A pandas dataframe with selected features.
     """
 
     raw_df = pd.read_csv("data/raw/fetal_health.csv")
 
-    if features:
-        return tuple(raw_df[feature].values for feature in features)
+    if as_df:
+        return raw_df[features or list(raw_df)]
 
-    return tuple(raw_df[feature].values for feature in list(raw_df))
+    return tuple(raw_df[feature].values for feature in features or list(raw_df))
