4f4216af49f47d03ebb95192a906227736c95399
Mikey Lau <mikey.lau@traininblocks.com>
2022-04-16 15:54:02 +0100

    feat: Shapiro

--- src/explore.py
+++ src/explore.py
@@ -8,6 +8,7 @@ from os.path import exists
 import dataframe_image as dfi
 import pandas as pd
 from pandas import DataFrame
+from scipy.stats import shapiro
 
 from tui import print_heading
 from visualise import FIGURE_DIR, PlotParams, plot_hist
@@ -50,19 +51,33 @@ def distribution_subplots() -> None:
                 f"{FIGURE_DIR}{filename}.png",
             )
 
-    for column in df.drop(columns=["fetal_health"], axis=1).columns:
+    for column in df.drop(columns=["fetal_health"], axis=1)[["baseline value"]].columns:
         X = [
             df[df["fetal_health"] == 1][column].values,
             df[df["fetal_health"] == 2][column].values,
             df[df["fetal_health"] == 3][column].values,
         ]
 
+        # Statistical analysis with test for normality
+        normality = shapiro(df[column].values)
+        df_describe = df[[column]].describe()
+        df_describe = pd.concat(
+            [
+                df_describe,
+                pd.DataFrame(
+                    [[normality.statistic]],
+                    columns=[column],
+                    index=pd.Index(["shapiro"]),
+                ),
+            ]
+        )
+
         data = PlotParams(
             title=f"Histogram distribution of {column.replace('_', ' ')}",
             x_label=f"{column.replace('_', ' ').capitalize()}",
             y_label="Frequency",
             x_values=X,
-            desc=df[[column]].describe(),
+            desc=df_describe,
             show=False,
         )
         plot_hist(
